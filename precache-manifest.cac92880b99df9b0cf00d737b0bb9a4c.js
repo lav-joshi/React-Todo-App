6690638cba39e72e7af5d31d6c1ec54b426c0fdf
lav-joshi <lcs2019022@iiitl.ac.in>
2020-07-27 06:07:58 +0530

    Updates

---
+++ precache-manifest.cac92880b99df9b0cf00d737b0bb9a4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9c75f54545f377f98847245e3de269",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "35b2502f8c4509366c28",
+    "url": "/React-Todo-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "53306736186071f2b809",
+    "url": "/React-Todo-App/static/css/main.3c71a374.chunk.css"
+  },
+  {
+    "revision": "35b2502f8c4509366c28",
+    "url": "/React-Todo-App/static/js/2.236349a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Todo-App/static/js/2.236349a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53306736186071f2b809",
+    "url": "/React-Todo-App/static/js/main.b811098c.chunk.js"
+  },
+  {
+    "revision": "0f00cbca3febd9bf0cac",
+    "url": "/React-Todo-App/static/js/runtime-main.3bfba492.js"
+  }
+]);
