c061b5b8cb968bc8b0543650a5f4c84c8ad0843a
lav-joshi <lcs2019022@iiitl.ac.in>
2020-07-27 07:41:02 +0530

    hi

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express=require("express");
-const app=express();
-
+const path=require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = process.env.PORT||4000;
 require("./db/mongoose");
+const app=express();
 app.use(cors());
 app.use(bodyParser.json());
+
+//server static assets if is in production 
+
+if(process.env.NODE_ENV === 'production') {
+
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname,"..","build","index.html"))
+    })
+}
+
 const todoRoutes = require("./routes/todos")
 app.get("/",(req,res)=>{
     res.send("Hi Lav");
